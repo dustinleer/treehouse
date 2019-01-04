0998f91af200da5e6ee52b885c712abdfaf5ffe2
Dustin Leer <dustin@wildcanon.com>
2019-01-04 15:34:28 -0500

    Combining Strings with concatention

--- php-courses/PHP Basics/strings.php
+++ php-courses/PHP Basics/strings.php
@@ -1,5 +1,27 @@
 <?php
+
+	/* Escape Sequence Example */
 	$name = 'Alena';
-	$string_one = "Learning to disply \"Hello $name!\" to the screen.\n";
+	$string_one = "Learning to display \"Hello $name!\" to the screen.\n";
+	
+	/* Combining Strings with concatentation */
+	$string_one = 'Learning to display ';
+	$string_one .= '"Hello ';
+	$string_one .= $name;
+	$string_one .= '!" to the screen.';
+	$string_one = $string_one . "\n";
+	// Prepend to a string
+	$string_one = 'I am ' . $string_one;
 	echo $string_one;
+
+	/* Concatentaion
+	Use		When				Example
+	""		Single variable		$myString = "Adding single variable $string to this string.";
+
+	.		Single line			$myString = 'Combining multiple' . $strings . 'on one line.";
+
+	.=		Multiple lines		$myString = 'Combining multiple lines';
+								$myString .= $strings;
+								$myString .= 'by using the assignment operator';
+	*/
 ?>
