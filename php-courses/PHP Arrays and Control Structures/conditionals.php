11fef0296621623c6534e65c78ce61434709f515
Dustin Leer <dustin@wildcanon.com>
2019-01-28 15:47:10 -0500

    Comments out conditional for new example

--- php-courses/PHP Arrays and Control Structures/conditionals.php
+++ php-courses/PHP Arrays and Control Structures/conditionals.php
@@ -2,13 +2,15 @@
 	$a = 10;
 	$b = 5;
 
-	if ( $a == $b ) {
-		echo ' values are equal ';
-	} else if ( $a < $b ) {
-		echo ' $a is less than $b ';
-	} else if ( $a > $b ) {
-		echo ' $a is greater than $b ';
-	} else {
-		echo ' values are not equal ';
-	}
+	// if ( $a == $b ) {
+	// 	echo ' values are equal ';
+	// } else if ( $a < $b ) {
+	// 	echo ' $a is less than $b ';
+	// } else if ( $a > $b ) {
+	// 	echo ' $a is greater than $b ';
+	// } else {
+	// 	echo ' values are not equal ';
+	// }
+
+	
 ?>
