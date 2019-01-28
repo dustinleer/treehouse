fa7156be75de6274e703db1c3bc069d9e08a0e5f
Dustin Leer <dustin@wildcanon.com>
2019-01-28 15:45:59 -0500

    Adds greater than conditional, yes there is repetitive code

--- php-courses/PHP Arrays and Control Structures/conditionals.php
+++ php-courses/PHP Arrays and Control Structures/conditionals.php
@@ -6,6 +6,8 @@
 		echo ' values are equal ';
 	} else if ( $a < $b ) {
 		echo ' $a is less than $b ';
+	} else if ( $a > $b ) {
+		echo ' $a is greater than $b ';
 	} else {
 		echo ' values are not equal ';
 	}
