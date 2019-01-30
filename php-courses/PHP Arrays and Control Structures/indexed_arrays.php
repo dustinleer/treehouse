54398c5d9e91047de9da7935a01480673927f014
Dustin Leer <dustin@wildcanon.com>
2019-01-30 14:31:25 -0500

    Learning about arrays
    array_push will add items to the end of an array
    array_unshift will add items to the beginning of an array

--- php-courses/PHP Arrays and Control Structures/indexed_arrays.php
+++ php-courses/PHP Arrays and Control Structures/indexed_arrays.php
@@ -1,8 +1,26 @@
 <?php
 
-	$learn = array( 'Conditonals', 'Arrays', 'Loops');
-	// var_dump( $learn );
+	$learn = array( 'Conditonals', 'Arrays', 'Loops' );
+	
+	// Adds an element to the end of an array
+	$learn[] = 'Build something awesome!';
+
+	// $learn = 'Build something awesome!';
+	/* !!! this will override the entire array !!! */
+
+	// Adds multiple elements to the end of an array
+	array_push( $learn, 'Functions', 'Forms', 'Objects' );
+
+	// Adds multiple elements to the beginning of an array
+	array_unshift( $learn, 'HTML', 'CSS' );
+
+	/* PHP Array functions documentation
+	 * http://php.net/manual/en/ref.array.php
+	 * read this to learn more about array functions
+	*/
+
+	var_dump( $learn );
 	// echo $learn[1];
 	// echo $learn;
-	echo implode("\n", $learn);
+	// echo implode("\n", $learn);
 ?>
