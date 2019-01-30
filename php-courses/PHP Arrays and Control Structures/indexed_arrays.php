b33d49475a8885026e9ac5dabce234d992de2bde
Dustin Leer <dustin@wildcanon.com>
2019-01-30 14:44:29 -0500

    Learning about removing Array elements

--- php-courses/PHP Arrays and Control Structures/indexed_arrays.php
+++ php-courses/PHP Arrays and Control Structures/indexed_arrays.php
@@ -1,5 +1,10 @@
 <?php
 
+	/* PHP Array functions documentation
+	 * http://php.net/manual/en/ref.array.php
+	 * read this to learn more about array functions
+	*/
+
 	$learn = array( 'Conditonals', 'Arrays', 'Loops' );
 	
 	// Adds an element to the end of an array
@@ -14,10 +19,21 @@
 	// Adds multiple elements to the beginning of an array
 	array_unshift( $learn, 'HTML', 'CSS' );
 
-	/* PHP Array functions documentation
-	 * http://php.net/manual/en/ref.array.php
-	 * read this to learn more about array functions
-	*/
+	// Shifts an element off the beginning of array.
+	// http://php.net/manual/en/function.array-shift.php
+	echo 'you removed ' . array_shift( $learn );
+
+	// Pops the element off the end of array
+	echo 'you removed ' . array_pop( $learn );
+
+	// Unset a given variable (works on more than array values, but does not update array keys
+	unset( $learn[1], $learn[2] );
+
+	// Reindexing array keys with the array_values function
+	$learn = array_values( $learn );
+
+	// unset($array); will remove your entire array.
+	unset( $learn );
 
 	var_dump( $learn );
 	// echo $learn[1];
