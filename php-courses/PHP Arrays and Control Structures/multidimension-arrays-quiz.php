8ff0aacc201f9d8ef739452ce84324143c9bd369
Dustin Leer <dustin@wildcanon.com>
2019-02-01 14:11:12 -0500

    Adds the final block to replace the hard coded text w/ the array values

--- php-courses/PHP Arrays and Control Structures/multidimension-arrays-quiz.php
+++ php-courses/PHP Arrays and Control Structures/multidimension-arrays-quiz.php
@@ -22,10 +22,13 @@
 
 	echo "<ul>\n";
 	//$contacts[0] will return 'Alena Holligan' in our simple array of names.
-	echo "<li>Alena Holligan : alena.holligan@teamtreehouse.com</li>\n";
-	echo "<li>Dave McFarland : dave.mcfarland@teamtreehouse.com</li>\n";
-	echo "<li>Treasure Porth : treasure.porth@teamtreehouse.com</li>\n";
-	echo "<li>Andrew Chalkley : andrew.chalkley@teamtreehouse.com</li>\n";
+		echo "<li>" . $contacts[0]["name"] . " : " . $contacts[0]["email"] . "</li>\n";
+		
+		echo "<li>" . $contacts[1]["name"] . " : " . $contacts[1]["email"] . "</li>\n";
+		
+		echo "<li>" . $contacts[2]["name"] . " : " . $contacts[2]["email"] . "</li>\n";
+		
+		echo "<li>" . $contacts[3]["name"] . " : " . $contacts[3]["email"] . "</li>\n";
 	echo "</ul>\n";
 
 ?>
