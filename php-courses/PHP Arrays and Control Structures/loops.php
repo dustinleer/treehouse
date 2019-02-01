0afb9572b47f61b4d7904fabf00c97699cd41e7c
Dustin Leer <dustin@wildcanon.com>
2019-02-01 16:11:50 -0500

    Learning more about for loops

--- php-courses/PHP Arrays and Control Structures/loops.php
+++ php-courses/PHP Arrays and Control Structures/loops.php
@@ -1,8 +1,8 @@
 <?php
 
-	for ( $year = date('Y') - 99; $year <= date('Y'); $year++ ) { 
-		echo $year . "<br /> \n";
-	}
+	// for ( $year = date('Y') - 99; $year <= date('Y'); $year++ ) { 
+	// 	echo $year . "<br /> \n";
+	// }
 
 	// while ( ++$year <= $currentYear ) {
 	// 	echo $year . "<br /> \n";
@@ -17,6 +17,10 @@
 	array_push( $learn, 'Functions', 'Forms', 'Objects' );
 	array_unshift( $learn, 'HTML', 'CSS' );
 	asort( $learn );
+
+	for ( $i = 0; $i < count( $learn ); $i++ ) {
+		echo $learn[$i] . "\n";
+	}
 
 	$count = 0;
 	// while ((list( $key, $val ) = each( $learn )) && $count++ < 2) {
