59659d5290e0330e12d707da8f5b800e2541d5a3
Dustin Leer <dustin@wildcanon.com>
2019-02-01 13:42:39 -0500

    Add short array syntax and combines arrays

--- php-courses/PHP Arrays and Control Structures/todo.php
+++ php-courses/PHP Arrays and Control Structures/todo.php
@@ -1,11 +1,20 @@
 <?php
 
-	$task1 = array(
+	$list[] = [
 		'title'    => 'Laundry',
 		'priority' => 2,
 		'due' 	   => '',
 		'complete' => true,
-	);
-	var_dump( $task1 );
+	];
+	$list[] = [
+		'title'    => 'Clean out refigorator',
+		'priority' => 3,
+		'due' 	   => '07/30/2016',
+		'complete' => false,
+	];
+	// $list = array( $task1, $task2 );
+	// var_dump( $list );
+	echo $list[0]['title'];
+	// var_dump( $task1 );
 
 ?>
