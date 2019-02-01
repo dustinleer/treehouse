b60414dcf4a851149b08c4351b1b2e07ab5a1c8b
Dustin Leer <dustin@wildcanon.com>
2019-02-01 14:00:47 -0500

    Adds Quiz Steps

---
+++ php-courses/PHP Arrays and Control Structures/multidimension-arrays-quiz.php
@@ -0,0 +1,14 @@
+<?php
+
+	//edit this array
+	$contacts = array('Alena Holligan', 'Dave McFarland', 'Treasure Porth', 'Andrew Chalkley');
+
+	echo "<ul>\n";
+	//$contacts[0] will return 'Alena Holligan' in our simple array of names.
+	echo "<li>Alena Holligan : alena.holligan@teamtreehouse.com</li>\n";
+	echo "<li>Dave McFarland : dave.mcfarland@teamtreehouse.com</li>\n";
+	echo "<li>Treasure Porth : treasure.porth@teamtreehouse.com</li>\n";
+	echo "<li>Andrew Chalkley : andrew.chalkley@teamtreehouse.com</li>\n";
+	echo "</ul>\n";
+
+?>
