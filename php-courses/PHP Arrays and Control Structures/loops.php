c5adc0bb55536443f90795247b320ad81399e883
Dustin Leer <dustin@wildcanon.com>
2019-02-01 16:25:12 -0500

    Finishes code challenge for "For Loops"

--- php-courses/PHP Arrays and Control Structures/loops.php
+++ php-courses/PHP Arrays and Control Structures/loops.php
@@ -27,6 +27,26 @@
 	// 	echo "$key => $val \n";
 	// }
 
+	/* PHP Arrays and Control Structures
+		* Create a FOR loop that displays each number from 1 to 100.
+		* Make sure that you DO NOT show 0 and you DO show 100.
+	*/
+	$facts = array(
+		57 => ' on Heinz ketchup bottles represents the number of varieties of pickles the company once had.',
+		2 => ' is the approximate hours a day Giraffes sleeps',
+		18 => ' is the average hours a Python sleeps per day',
+		10 => ' per cent of the world is left-handed.',
+		11 => ' Empire State Buildings, stacked one on top of the other, would be required to measure the Gulf of Mexico at its deepest point.',
+		98 => '% of the atoms in your body are replaced every year',
+		69 => ' is the largest number of recorded children born to one woman',
+	);
+
+	for ( $i = 1; $i <= 100; $i++ ) {
+		echo $i  . "\n";
+		if ( isset( $facts[$i] ) ) {
+			echo $i . $facts[$i] . "\n";
+		}
+	}
 
 
 ?>
