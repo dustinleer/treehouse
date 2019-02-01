da148be09c457b5ac9a52176de5dc5be54385041
Dustin Leer <dustin@wildcanon.com>
2019-02-01 16:53:00 -0500

    Learning about foreach loops

--- php-courses/PHP Arrays and Control Structures/todo.php
+++ php-courses/PHP Arrays and Control Structures/todo.php
@@ -1,20 +1,37 @@
 <?php
 
-	$list[] = [
-		'title'    => 'Laundry',
-		'priority' => 2,
-		'due' 	   => '',
-		'complete' => true,
-	];
-	$list[] = [
-		'title'    => 'Clean out refigorator',
-		'priority' => 3,
-		'due' 	   => '07/30/2016',
-		'complete' => false,
-	];
-	// $list = array( $task1, $task2 );
-	var_dump( $list );
+	include 'list.php';
+	
+	foreach ( $list as $key => $item) {
+		echo $key . ' = ' . $item['title'] . "<br />\n";
+	}
+
+	echo  '<table>';
+		echo '<tr>';
+			echo '<th>Title</th>';
+			echo '<th>Priority</th>';
+			echo '<th>Due Date</th>';
+			echo '<th>Complete</th>';
+		echo '</tr>';
+
+		foreach ( $list as $item ) {
+			echo '<tr>';
+				echo '<td>' . $item['title'] . "</td>\n";	
+				echo '<td>' . $item['priority'] . "</td>\n";	
+				echo '<td>' . $item['due'] . "</td>\n";	
+				echo '<td>';
+					if ( $item['complete'] ) {
+						echo 'Yes';
+					} else {
+						echo 'No';
+					}
+				echo "</td>\n";	
+			echo '</tr>';
+		}
+
+	echo  '</table>';
+
+	//	var_dump( $list );
 	// echo $list[0]['title'];
-	// var_dump( $task1 );
 
 ?>
