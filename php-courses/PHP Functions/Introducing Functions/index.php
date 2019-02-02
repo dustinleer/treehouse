a690395ea3b4f747727a077c475cf7cebdb6747d
Dustin Leer <dustin@wildcanon.com>
2019-02-02 06:51:55 -0500

    PHP Variable Functions

--- php-courses/PHP Functions/Introducing Functions/index.php
+++ php-courses/PHP Functions/Introducing Functions/index.php
@@ -9,17 +9,17 @@
 	// hello();
 
 
-	$current_user = 'Mike';
+	// $current_user = 'Mike';
 
-	function is_mike() {
-		global $current_user;
-		if ( $current_user == 'Mike' ) {
-			echo 'It is Mike!';
-		} else {
-			echo 'Nope, it is not Mike.';
-		}
-	}
-	is_mike();
+	// function is_mike() {
+	// 	global $current_user;
+	// 	if ( $current_user == 'Mike' ) {
+	// 		echo 'It is Mike!';
+	// 	} else {
+	// 		echo 'Nope, it is not Mike.';
+	// 	}
+	// }
+	// is_mike();
 
 	
 	// function hello( $arr ) {
@@ -38,38 +38,50 @@
 	// );
 	// hello($names);
 
-	function get_info( $name, $title = Null ) {
-		if ( $title ) {
-			echo "$name has arrived, the are with us as a $title";
-		} else {
-			echo "$name has arrived, welcome!";
-		}
-	}
-	get_info('Mike', 'frog');
+	// function get_info( $name, $title = Null ) {
+	// 	if ( $title ) {
+	// 		echo "$name has arrived, the are with us as a $title";
+	// 	} else {
+	// 		echo "$name has arrived, welcome!";
+	// 	}
+	// }
+	// get_info('Mike', 'frog');
 
-	function hello( $name ) {
-		if ( $name == 'Mike' ) {
-			return 'Hello, Mike!';
-		} else {
-			return 'Hello, stranger.';
-		}
-	}
-	$greeting = hello( 'Mike' );
+	// function hello( $name ) {
+	// 	if ( $name == 'Mike' ) {
+	// 		return 'Hello, Mike!';
+	// 	} else {
+	// 		return 'Hello, stranger.';
+	// 	}
+	// }
+	// $greeting = hello( 'Mike' );
 
-	echo $greeting;
+	// echo $greeting;
 
 	// Returning Integers
-	function add_up( $a, $b ) {
-		$arr = array(
-			$a,
-			$b,
-			$a + $b,
-		);
-		return $arr;
-	}
-	$value = add_up( 2, 4 );
+	// function add_up( $a, $b ) {
+	// 	$arr = array(
+	// 		$a,
+	// 		$b,
+	// 		$a + $b,
+	// 	);
+	// 	return $arr;
+	// }
+	// $value = add_up( 2, 4 );
 
-	// print_r( $value );
-	echo $value[2];
+	// // print_r( $value );
+	// echo $value[2];
+
+	function answer() {
+		return 42;
+	}
+
+	function add_up( $a, $b ) {
+		return $a + $b;
+	}
+
+	$func = 'add_up';
+	$num = $func( 5, 10 );
+	echo $num;
 
 ?>
