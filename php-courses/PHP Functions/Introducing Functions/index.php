8c70a5551bcec841b0945c1e80823efc14efb0da
Dustin Leer <dustin@wildcanon.com>
2019-02-05 13:41:32 -0500

    Learning about Array Functions

--- php-courses/PHP Functions/Introducing Functions/index.php
+++ php-courses/PHP Functions/Introducing Functions/index.php
@@ -116,6 +116,31 @@
 	// Will return false if 'string' is not found.
 
 	$start = strpos( $phrase, 'hit' );
-	echo substr( $phrase, $start );
+	// echo substr( $phrase, $start );
+
+
+	/* PHP Functions
+	 *  PHP Array Functions
+	*/
+
+	// array_keys
+	$names = array(
+		'Mike' => 'Frog',
+		'Chris' => 'Teacher',
+		'Hampton' => 'Teacher',
+	);
+
+	// foreach (array_keys( $names ) as $name) {
+	// 	echo "Hello, $name <br>";
+	// };
+
+
+	// array_walk
+	
+	function print_info( $value, $key ) {
+		echo "$key is a $value.<br>";
+	}
+	
+	array_walk( $names, 'print_info' );
 
 ?>
