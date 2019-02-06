7a5f50e0e0f9bec19c9454f3a3cc0b8de752b555
Dustin Leer <dustin@wildcanon.com>
2019-02-05 20:34:19 -0500

    Adds variables for title and active link states in the nav

---
+++ php-courses/PHP Basic Site/s1v1/catalog.php
@@ -0,0 +1,31 @@
+<?php
+	$pageTitle = "Full Catalog";
+	$section = null;
+
+	if ( isset( $_GET["cat"] ) ) {
+		if ( $_GET["cat"] == "books" ) {
+			$pageTitle = "Books";
+			$section = "books";
+		} else if ( $_GET["cat"] == "movies" ) {
+			$pageTitle = "Movies";
+			$section = "movies";
+		} else if ( $_GET["cat"] == "music" ) {
+			$pageTitle = "Music";
+			$section = "music";
+		}
+	}
+
+	include( 'inc/header.php' );
+?>
+
+		<div class="section page">
+
+			<h1><?= $pageTitle; ?></h1>
+
+		</div>
+
+	</div> <!-- end of content -->
+
+<?php
+	include( 'inc/footer.php');
+?>
