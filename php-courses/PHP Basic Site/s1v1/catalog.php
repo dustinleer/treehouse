4109f3a935757c1e9cf22d057377e5adc952bf7d
Dustin Leer <dustin@wildcanon.com>
2019-03-08 20:19:04 -0500

    Adds sorting and a link to go to catalog page

--- php-courses/PHP Basic Site/s1v1/catalog.php
+++ php-courses/PHP Basic Site/s1v1/catalog.php
@@ -1,23 +1,23 @@
 <?php
 
-include("inc/data.php");
-include("inc/functions.php");
+	include("inc/data.php");
+	include("inc/functions.php");
 
-$pageTitle = "Full Catalog";
-$section = null;
+	$pageTitle = "Full Catalog";
+	$section = null;
 
-if (isset($_GET["cat"])) {
-    if ($_GET["cat"] == "books") {
-        $pageTitle = "Books";
-        $section = "books";
-    } else if ($_GET["cat"] == "movies") {
-        $pageTitle = "Movies";
-        $section = "movies";
-    } else if ($_GET["cat"] == "music") {
-        $pageTitle = "Music";
-        $section = "music";
-    }
-}
+	if ( isset( $_GET["cat"] ) ) {
+		if ( $_GET["cat"] == "books" ) {
+			$pageTitle = "Books";
+			$section = "books";
+		} else if ( $_GET["cat"] == "movies" ) {
+			$pageTitle = "Movies";
+			$section = "movies";
+		} else if ( $_GET["cat"] == "music" ) {
+			$pageTitle = "Music";
+			$section = "music";
+		}
+	}
 
 	include( 'inc/header.php' );
 ?>
@@ -26,7 +26,12 @@ if (isset($_GET["cat"])) {
 
 			<div class="wrapper">
 			
-				<h1><?= $pageTitle; ?></h1>
+				<h1><?php
+					if( $section != null ) {
+						echo "<a href='catalog.php'>Full Catalog</a> &gt; ";
+					}
+					echo $pageTitle;
+				?></h1>
 
 				<ul class="items">
 					<?php
